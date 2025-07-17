162fe51f9d3dd7c7bbba0db20c9d85f994381f59
2668A <peiyi.a.wang@gmail.com>
2025-07-16 19:11:14 -0500

    deleting old files

--- GPS and Inertial/GPS and Inertial/include/autons.hpp
+++
@@ -1,9 +0,0 @@
-/* External declarations and variables */
-/* Only modify when needed */
-#pragma once
-#include "EZ-Template/drive/drive.hpp"
-extern Drive chassis;
-void default_constants();
-
-
-// Add autonomous function declarations here
